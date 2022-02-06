181c0b7bdd78448711dd50170fad6cef8638dfc7
Luis Montealegre <montealegreluis@gmail.com>
2022-02-05 21:42:23 -0600

    test: Move test to the right package
    - `DomainExceptionTest.java` was not in the same test package as its
      production counterpart
    - Add missing test case to `DomainExceptionTest`

--- src/test/java/com/montealegreluis/servicebuses/DomainExceptionTest.java
+++ src/test/java/com/montealegreluis/servicebuses/DomainExceptionTest.java
@@ -1,8 +1,7 @@
-package com.montealegreluis.servicebuses.commandbus.middleware.error;
+package com.montealegreluis.servicebuses;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.montealegreluis.servicebuses.DomainException;
 import org.junit.jupiter.api.Test;
 
 final class DomainExceptionTest {
@@ -13,9 +12,22 @@ final class DomainExceptionTest {
     assertEquals("expired-tax-form", exception.code());
   }
 
+  @Test
+  void it_knows_its_cause_exception() {
+    var cause = new RuntimeException("Internal Server Error");
+    var exception = new ExpiredTaxForm("Provided tax form is expired", cause);
+
+    assertEquals("expired-tax-form", exception.code());
+    assertEquals(cause, exception.getCause());
+  }
+
   private static class ExpiredTaxForm extends DomainException {
     public ExpiredTaxForm(String message) {
       super(message);
+    }
+
+    public ExpiredTaxForm(String message, Throwable cause) {
+      super(message, cause);
     }
   }
 }
