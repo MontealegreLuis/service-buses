42a15648644b97bea45ced99651c873c20b6d605
Luis Montealegre <montealegreluis@gmail.com>
2022-02-05 22:05:31 -0600

    feat!: Remove middleware code
    BREAKING CHANGE: Middleware buses were moved to their own repository:
    https://github.com/MontealegreLuis/service-buses-middleware

--- src/main/java/com/montealegreluis/servicebuses/commandbus/middleware/CommandMiddleware.java
+++
@@ -1,9 +0,0 @@
-package com.montealegreluis.servicebuses.commandbus.middleware;
-
-import com.montealegreluis.servicebuses.ActionException;
-import com.montealegreluis.servicebuses.commandbus.Command;
-import com.montealegreluis.servicebuses.commandbus.CommandHandler;
-
-public interface CommandMiddleware {
-  void execute(Command command, CommandHandler<Command> next) throws ActionException;
-}
